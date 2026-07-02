06b3f87bc0ad04b0fd6681dbb211d31502451890
Никита Атамас <nikitoatamas@Nikita.local>
2026-07-02 19:52:07 +0300

    Improve landing reference prompt feature

--- landing-media.js
+++ landing-media.js
@@ -1,5 +1,5 @@
 (function () {
-  const MANIFEST_URL = "/assets/landing/manifest.json?v=20260612-landing-5";
+  const MANIFEST_URL = "/assets/landing/manifest.json?v=20260702-landing-ref-1";
 
   const HERO_FALLBACK = [
     { src: "https://picsum.photos/seed/kartivio-1/420/560", alt: "Пример генерации 1" },
